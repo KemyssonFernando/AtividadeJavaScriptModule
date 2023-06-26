b7939b81db850041f2a7b22ecc2bc3be552bc142
KemyssonFernando <130847061+KemyssonFernando@users.noreply.github.com>
2023-06-26 16:39:19 -0300

    atividade de fixação turma-1
    e também segue a atividade que foi passada pra exemplo de cada 2 vezes ta as duas juntas

---
+++ atividade 2 variaveis de cada.js
@@ -0,0 +1,43 @@
+var numero = 1000;
+console.log(numero);
+
+var negativo =-1000;
+console.log(negativo);
+
+var nome_da_mae = 'josi';
+console.log(nome_da_mae);
+
+var nome_do_pai = 'fabio';
+console.log (nome_do_pai);
+
+var verdade = true
+var falso = false
+
+console.log ("o carro é seu ?");
+console.log (falso);
+
+console.log('o carro é do meu pai!!');
+console.log(verdade);
+
+var carro = ["amortecedor","rodas","capo"];
+var moto  =['raio','guidom','descanso'];
+console.log (moto);
+console.log (carro);
+var carro1 = {modelo:'volksvagem',ano:2070,cor:'rosa'};
+console.log(carro1);
+
+var moto1 = {modelo:'bros',ano:2017,cor:'vermelha'};
+console.log(moto1);
+console.log(moto1.ano);
+
+var minha_conta;
+console.log('minha_conta: ' + minha_conta);
+
+var conta_bancaria;
+console.log ('conta_bancaria: '+ conta_bancaria);
+
+var sem_valor = null;
+console.log('sem_valor: '+ sem_valor);
+
+var sem_dinheiro = null;
+console.log('sem_dinheiro: '+ sem_dinheiro);
